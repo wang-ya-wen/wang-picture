145aa3f2bfadbd527c33883c9bba553a09e6c2bc
wang <19137539336@163.com>
2025-12-09 14:05:18 +0800

    '基础功能代码'

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -59,3 +59,32 @@ create table if not exists picture
 
 ) comment '图片' collate =utf8mb4_unicode_ci;
 
+ALTER TABLE picture
+       ADD COLUMN spaceId bigint null comment '空间id(为空表示公共空间';
+CREATE INDEX idx_spaceId ON picture(spaceId);
+
+create table space
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    spaceName  varchar(128)                       null comment '空间名称',
+    spaceLevel int      default 0                 null comment '空间级别:0-普通版 1-专业版 2-旗舰版',
+    maxSize    bigint   default 0                 null comment '空间图片的最大总大小',
+    maxCount   bigint   default 0                 null comment '空间图片的最大数量',
+    totalSize  bigint   default 0                 null comment '当前空间下图片的总大小',
+    userId     bigint                             not null comment '创建用户的id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    editTime   datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    spaceId    mediumtext                         null comment '空间的id'
+);
+
+create index idx_spaceLevel
+    on space (spaceLevel);
+
+create index idx_spaceName
+    on space (spaceName);
+
+create index idx_userId
+    on space (userId);
